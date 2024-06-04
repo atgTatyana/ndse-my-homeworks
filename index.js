7c0a3a9df2ffbe6ce1ceb42558c75a017efc9f50
Tatyana Amosova <atgatg@bk.ru>
2024-06-04 18:11:58 +0300

    Init

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express')
-const mongoose = require('mongoose')
 
 const session = require('express-session')
 const passport = require('passport')
-
 const local = require('./passport/login')(passport)
 const signup = require('./passport/signup')(passport)
-
 const init = require('./passport/init')(passport)
 
 const errorMiddleware = require('./middleware/error')
@@ -14,33 +11,45 @@ const errorMiddleware = require('./middleware/error')
 const indexRouter = require('./routes/index')
 const authRouter = require('./routes/auth')(passport)
 
+const http = require('http')
+const socketIO = require('socket.io')
+
 const app = express()
+const server = http.Server(app)
+const io = socketIO(server)
 
 app.use(express.json())
 app.set('view engine', 'ejs')
 app.use(express.urlencoded())
 
-app.use(session({ secret: 'SECRET'}));
-  
+app.use(session({ secret: 'SECRET'}))  
 app.use(passport.initialize())
 app.use(passport.session())
 
 app.use('/api/user', authRouter)
 app.use('/api/books', indexRouter)
 
+io.on('connection', (socket) => {   // при наступлении события connection
+    const {id} = socket
+    console.log(`Socket connected: ${id}`)
+
+    const {roomName} = socket.handshake.query  
+    console.log(`Socket roomName: ${roomName}`)
+    socket.join(roomName)       // подписываемся на события данной комнаты
+    socket.on('message-to-room', (msg) => {
+        msg.type = `room: ${roomName}`
+        socket.to(roomName).emit('message-to-room', msg)   // отправка сообщения в комнату
+        socket.emit('message-to-room', msg)
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`Socket disconnected: ${id}`)
+    })
+})
+
 app.use(errorMiddleware)
 
-async function start(PORT, UrlDB) {     // запуск приложения
-    try {
-        await mongoose.connect(UrlDB)      // подключение к БД
-        app.listen(PORT, () => {        // запускаем наш сервер
-            console.log(`Server listening on port ${PORT}.`)
-        })        
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-const UrlDB = process.env.UrlDB
 const PORT = process.env.PORT || 3000
-start(PORT, UrlDB)
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}.`)
+})   
--- routes/index.js
+++ routes/index.js
@@ -1,76 +1,102 @@
 const express = require('express')
-const Books = require('../models/books')
+const path = require('path')
+const { v4: uuid } = require('uuid')
+
+class Book {
+  constructor(title, description, authors, favorite, fileCover, fileName, fileBook, id = uuid()) {
+    this.title = title,
+    this.description = description,
+    this.authors = authors,
+    this.favorite = favorite,
+    this.fileCover = fileCover,
+    this.fileName = fileName,
+    this.id = id
+  }
+}
+
+const books = [
+  {
+    title: "Доктор Живаго", 
+    description: "Через трагическую историю жизни главного героя, попавшего в чудовищный круговорот исторических событий - революция, Гражданская война, - показана жизнь как таковая, её глубинные потайные механизмы, её загадочная суть, её взлёты и падения.",
+    authors: "Б. Пастернак",
+  },
+  {
+    title: "Мастер и Маргарита", 
+    description: "История о дьяволе и его свите, почтивших своим присутствием Москву 1930-х, о прокураторе Иудеи всаднике Понтии Пилате и нищем философе Иешуа Га-Ноцри, о талантливом и несчастном Мастере и его прекрасной и верной возлюбленной Маргарите.",
+    authors: "М. Булгаков",
+  }
+]
+
+const library = []
+
+books.map((el, idx) => {
+  const newBook = new Book(`book ${el.title}`, `desc ${el.description}`, `authors ${el.authors}`,
+    `favorite ${idx}`, `fileCover ${idx}`, `fileName ${idx}`)
+  library.push(newBook)
+})
 
 const router = express.Router()
 
-router.get('/', async (req, res) => {     // получить все записи
-  try {
-    const books = await Books.find().select('-__v')
-    res.json(books)
-  } catch (e) {
-    res.status(500).json(e)
+router.get('/', (req, res) => {    // получить все записи
+  res.json(library) 
+})
+
+router.get('/:id', (req, res) => {    // получение единственной записи по id
+  const {id} = req.params
+  const idx = library.findIndex(el => el.id === id)
+  
+  if( idx !== -1) {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+
+  } else {
+    res.status(404)
+    res.json('404 | книга не найдена')
   }
 })
   
-router.get('/:id', async (req, res) => {    // получение единственной записи по id
-  const {id} = req.params
+router.post('/', (req, res) => {    // создание новой записи
+  const {title, description, authors, favorite, fileCover, fileName} = req.body
+  const newBook = new Book(title, description, authors, favorite, fileCover, fileName)
+  library.push(newBook)
   
-  try {
-    const book = await Books.findById(id).select('-__v')
-    
-    if (book === null) {    // если нет такого id
-      res.status(404)
-      res.json('404 | книга не найдена')
-      
-    } else {
-      res.json(book)
+  res.status(201)
+  res.json(newBook)
+})
+
+router.put('/:id', (req, res) => {    // обновление записи
+  const {title, description, authors, favorite, fileCover, fileName} = req.body
+  const {id} = req.params
+  const idx = library.findIndex(el => el.id === id)
+  
+  if (idx !== -1){
+    library[idx] = {
+      ...library[idx],
+      title,
+      description,
+      authors,
+      favorite,
+      fileCover,
+      fileName
     }
-  } catch (e) {
-    res.status(500).json(e)
+  
+    res.json(library[idx])
+  } else {
+    res.status(404)
+    res.json('404 | книга не найдена')
   }
 })
 
-router.post('/', async (req, res) => {    // создание новой записи
-  const {title, description, authors, favorite, fileCover, fileName} = req.body
-
-  const newBook = new Books({
-    title, 
-    description,
-    authors,
-    favorite,
-    fileCover,
-    fileName,
-  })
-
-  try {
-    await newBook.save()       // сохранить новую запись в базу
-    res.json(newBook)
-  } catch (e) {
-    res.status(500).json(e)
-  }
-})
-
-router.put('/:id', async (req, res) => {    // обновление записи
-  const {title, description, authors, favorite, fileCover, fileName} = req.body
+router.delete('/:id', (req, res) => {     // удаление записи
   const {id} = req.params
-
-  try {
-    await Books.findByIdAndUpdate(id, {title, description, authors, favorite, fileCover, fileName})
-    res.redirect(`/api/books/${id}`)
-  } catch (e) {
-    res.status(500).json(e)
-  }  
+  const idx = library.findIndex(el => el.id === id)
+       
+  if(idx !== -1){
+    library.splice(idx, 1)
+    res.json('ok')
+  } else {
+    res.status(404)
+    res.json('404 | книга не найдена')
+  }
 })
   
-router.delete('/:id', async (req, res) => {   // удаление записи
-  const {id} = req.params
-    
-  try {
-    await Books.deleteOne({_id: id})     // фильтр для удаления
-    res.json('ok')
-  } catch (e) {
-    res.status(500).json(e)
-  }
-})
-
 module.exports = router
